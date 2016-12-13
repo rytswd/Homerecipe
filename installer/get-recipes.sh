8ced4081ca4717811f3aead4b9c7f5998e90ff36
Ryota <rytswd@gmail.com>
2016-12-13 01:00:30 +0000

    Add brew bundle solution which handles both brew and cask

--- installer/get-recipes.sh
+++ installer/get-recipes.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # Temporary shell script to run bunch of scripts
 
-# Get brew recipe
-brew leaves > brew-recipe
-# for i in `cat brew-recipe`; do brew install $i; done
+# Get brew bundle (includes cask)
+brew bundle dump --force --file=recipes/brew-recipe
+# brew bundle --file=recipes/brew-recipe
 
 # Get npm global recipe
-# TODO: Add npm equivalent
+# TODO: This is a workaround - there must be a better way...
+\ls `npm root -g` > recipes/npm-recipe
+# for i in `cat recipes/npm-recipe`; do npm install -g $i; done
