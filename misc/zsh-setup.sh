c2f9044ceaff4cc62f7107c332eb6feae17a0a3f
Ryota <rytswd@gmail.com>
2017-03-12 18:51:02 +0000

    Update zsh installation to manual installation rather than cURL

--- misc/zsh-setup.sh
+++ misc/zsh-setup.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 # Downloads oh-my-zsh https://github.com/robbyrussell/oh-my-zsh
-#   confirmed as of March 2017
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# The below shell script assumes you are running interactively,
+#   which is not ideal to wrap it with other scripts
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+chsh -s "$(grep /zsh$ /etc/shells | tail -1)"
 
 # Manual workaronud - it downloads some custom plugins and
 #   add them in ~/.oh-my-zsh/ directory
-git clone git://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM"/plugins/zsh-autosuggestions
+git clone git://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
