81b70a506c3c5da6a4d9a657571c425e2ac15de7
Ryota <rytswd@gmail.com>
2017-03-16 00:49:28 +0000

    Add nvm specific items

---
+++ custom/nvm/nvm-setup.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Managing nvm via Homebrew is unsupported
+#   `brew info nvm` for more information
+
+# Recommended installation:
+#curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+
+# WARNING: Default recipe does use Homebrew for simplicity sake
+
+[[ -f ~/.nvm ]] || mkdir ~/.nvm
