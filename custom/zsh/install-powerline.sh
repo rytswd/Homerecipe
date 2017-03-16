06e4e77c5a990041fbd7ce5ef989c85212a8af59
Ryota <rytswd@gmail.com>
2017-03-16 00:00:30 +0000

    Add temporary skip logic

--- custom/zsh/install-powerline.sh
+++ custom/zsh/install-powerline.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/powerline/fonts.git /tmp/fonts
+PL_FONTS_SHA='e13f86bf4dbd34495510694ead9ba76f4241d2e7'
+PL_FONTS_DIR="$HOME/Library/Fonts" # macOS location
+
+sha=$(test -f "${PL_FONTS_DIR}" && tar -c "${PL_FONTS_DIR}" | openssl sha1)
+[[ "$PL_FONTS_SHA" == "$sha" ]] || { echo "Powerline Fonts are already installed. Skipping."; exit 0; } 
+git clone https://github.com/powerline/fonts.git /tmp/powerline-fonts
 /tmp/fonts/install.sh
-#rm -rf /tmp/fonts
+rm -rf /tmp/powerline-fonts
--- custom/zsh/zsh-setup.sh
+++ custom/zsh/zsh-setup.sh
@@ -5,6 +5,8 @@
 # The below shell script assumes you are running interactively,
 #   which is not ideal to wrap it with other scripts
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+[[ -d ~/.oh-my-zsh ]] && { echo ".oh-my-zsh already exists, skipping zsh setup"; exit 0; }
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 chsh -s "$(grep /zsh$ /etc/shells | tail -1)"
 
