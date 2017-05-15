4f24fa962492b0c0208925b0e1aa3bfe2dfeb758
Ryota <rytswd@gmail.com>
2017-05-15 01:38:13 +0100

    Reorder calls

--- core/scripts/util.sh
+++ core/scripts/util.sh
@@ -7,19 +7,22 @@
 #  # shellcheck disable=SC1091,SC1090
 #  source "${HOMERECIPE_SCRIPTS}"/util.sh
 
-# HOMERECIPE_SCRIPTS and HOMERECIPE_DIR need to be defined
-# Assumed "cook" is called to run this script
-[[ ! -z $HOMERECIPE_DIR ]] || { echo "\$HOMERECIPE_DIR is not found. Process exiting."; exit 1; }
-[[ ! -z $HOMERECIPE_SCRIPTS ]] || { echo "\$HOMERECIPE_SCRIPTS is not found. Process exiting."; exit 1; }
-
-[[ ! -z $DEBUG ]] && { echo "\$DEBUG is set, log verbosity is set high"; }
-
 # shellcheck disable=SC1090
 # For first time run, it is possible that .homerecipe isn't loaded during the startup.
 # If the file is found in home directory, this tries loading variables from it.
 [[ -L "$HOME"/.homerecipe ]] && source "$HOME"/.homerecipe
 
-# Define utility functions
+#[[ ! -z $DEBUG ]] && { echo "\$DEBUG is set, log verbosity is set high"; }
+
+## Validations
+
+# HOMERECIPE_SCRIPTS and HOMERECIPE_DIR need to be defined
+# Assumed "cook" is called to run this script
+[[ ! -z $HOMERECIPE_DIR ]] || { echo "\$HOMERECIPE_DIR is not found. Process exiting."; exit 1; }
+[[ ! -z $HOMERECIPE_SCRIPTS ]] || { echo "\$HOMERECIPE_SCRIPTS is not found. Process exiting."; exit 1; }
+
+## Utility Functions
+
 confirm() {
   # call with a prompt string or use a default
   read -r -p "${1:-Are you sure? [y/N]} " response
